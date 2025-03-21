dc1e602416bf535a49a3d08080ae064e7c428fff
Muhammad Shahriar Ebrahim <md_shahriar_ebrahim@outlook.com>
2025-03-21 23:31:21 +0600

    Added solution for Elephant problem (elephant.c)

---
+++ Difficulty_800/elephant.c
@@ -0,0 +1,32 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * *
+ * █████████████████████████████████████████████ *
+ * █                                           █ *
+ * █  Name    : Muhammad Shahriar Ebrahim      █ *
+ * █  Country : Bangladesh                     █ *
+ * █  Language: C                              █ *
+ * █  Problem : Elephant                       █ *
+ * █  Date    : 21/03/2025                     █ *
+ * █                                           █ *
+ * █████████████████████████████████████████████ *
+ * * * * * * * * * * * * * * * * * * * * * * * * */
+
+ #include <stdio.h>
+
+    int main() {
+        long int x;
+        long int s = 0;
+        scanf("%ld", &x);
+        if (x < 5) {
+            printf("1");
+        }
+        else if (x >= 5) {
+            s = x / 5;
+            if (s % 5 == 0) {
+                printf("%ld\n", s);
+            }
+            else {
+                printf("%ld\n", s+1);
+        }
+        
+        }
+    }
