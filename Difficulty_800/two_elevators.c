cf8dd064e3d42d16d896187fdbac7f0e48e6ce0e
shahriarEBRAHIM <shahriarebrahim2021@gmail.com>
2025-03-26 03:29:41 +0600

    Added solution for Two Elevators problem

---
+++ Difficulty_800/two_elevators.c
@@ -0,0 +1,38 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * *
+ * █████████████████████████████████████████████ *
+ * █                                           █ *
+ * █  Name    : Muhammad Shahriar Ebrahim      █ *
+ * █  Country : Bangladesh                     █ *
+ * █  Language: C                              █ *
+ * █  Problem : Two Elevators                  █ *
+ * █  Date    : 20/03/2025                     █ *
+ * █                                           █ *
+ * █████████████████████████████████████████████ *
+ * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+    int main() {
+        long long int t, a, b, c, i = 0, a_time = 0, b_time = 0;
+        scanf("%lld", &t);
+        do {
+            scanf("%lld %lld %lld", &a, &b, &c);
+            a_time = a - 1;
+            if (c == 1) {
+                b_time = llabs(b - c);
+            }
+            else {
+                b_time = llabs(b - c) + llabs(c - 1);
+            }
+            if (a_time < b_time) {
+                printf("1\n");
+            } else if (a_time > b_time) {
+                printf("2\n");
+            } else {
+                printf("3\n");
+            }
+            i++;
+        } while (i < t);
+        return 0;
+    }
