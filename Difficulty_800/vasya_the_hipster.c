e676040119ce2538419ad491b96431013f519757
shahriarEBRAHIM <shahriarebrahim2021@gmail.com>
2025-03-26 03:57:02 +0600

    Added solution for Vasya The Hipster problem

---
+++ Difficulty_800/vasya_the_hipster.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+    int main () {
+        int r, b, df = 0, dn = 0;
+        scanf("%d %d", &r, &b);
+        if (r > b) {
+            df = b;
+            printf("%d ", df);
+            dn = (r - b) / 2;
+            printf("%d\n", dn);
+        } 
+        else if (b > r) {
+            df = r;
+            printf("%d ", df);
+            dn = (b - r) / 2;
+            printf("%d\n", dn);
+        }
+        else {
+            df = r;
+            printf("%d ", df);
+            dn = 0;
+            printf("%d\n", dn);
+        }
+        return 0;
+    }
